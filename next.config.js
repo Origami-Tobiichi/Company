27fe3437b7bf5340090c747fdcf6d234ee21b5e3
Origami-Tobiichi <cyberdark454@gmail.com>
2026-06-25 14:09:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['*.vercel-storage.com'] },
-  experimental: { serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'] },
-}
-module.exports = nextConfig
+  images: {
+    domains: ['*.vercel-storage.com'],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
+  webpack: (config, { isServer }) => {
+    // Menangani modul 'fs' dan 'encoding' yang digunakan oleh face-api.js
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        encoding: false,
+        ...config.resolve.fallback,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
